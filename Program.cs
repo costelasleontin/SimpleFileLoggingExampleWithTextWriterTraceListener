9039638f6d243f72c299bbaca2d65b5c9e7a120b
Costelas Leontin <costelasleontin@yahoo.com>
2023-02-18 13:38:53 +0200

    Added better formating to the code lines

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 ﻿using System.Diagnostics;
 
-string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "log12345.txt"); //set the path to the file where the loggs should be written
+//set the path to the file where the loggs should be written
+string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "log.txt"); 
+
 Console.WriteLine($"Writing to: {logPath}");
-TextWriterTraceListener logFile = new(File.CreateText(logPath)); //create TraceListener
-Trace.Listeners.Add(logFile); //add TraceListener to listeners
+//create TraceListener
+TextWriterTraceListener logFile = new(File.CreateText(logPath)); 
+//add TraceListener to listeners
+Trace.Listeners.Add(logFile); 
+
 // text writer is buffered, so this option calls
 // Flush() on all listeners after writing
 Trace.AutoFlush = true;
-Debug.WriteLine("Debug says, I am watching!"); //this displays debug message but only in Debug configuration
-Trace.WriteLine("Trace says, I am watching!"); //efectivelly writing trace to log
+
+//this displays debug message but only in Debug configuration
+Debug.WriteLine("Debug says, I am watching!"); 
+
+//efectivelly writing trace to log
+Trace.WriteLine("Trace says, I am watching!"); 
